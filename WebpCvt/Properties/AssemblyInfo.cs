dd1810882aed19ee22dce4b9ddf46692309090f5
hrpzcf <58855572+hrpzcf@users.noreply.github.com>
2024-02-23 00:06:52 +0800

    版本：1.1.0

--- WebpCvt/Properties/AssemblyInfo.cs
+++ WebpCvt/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("WebpCvt")]
-[assembly: AssemblyDescription("用于监视目录，自动把所监视的目录里新增的 webp 文件转换为 jpeg/png/bmp 文件")]
+[assembly: AssemblyDescription("用于监视目录，自动把所监视的目录里新增的 webp 文件转换为 jpeg/png 文件")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("hrpzcf")]
 [assembly: AssemblyProduct("WebpCvt")]
@@ -51,5 +51,5 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
