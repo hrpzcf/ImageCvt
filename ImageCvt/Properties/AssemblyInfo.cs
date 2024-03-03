8d25f72c1deee96d30dfe4ddbe9f508ea36913c4
hrpzcf <58855572+hrpzcf@users.noreply.github.com>
2024-03-04 01:00:39 +0800

    更新程序描述

--- ImageCvt/Properties/AssemblyInfo.cs
+++ ImageCvt/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("ImageCvt")]
-[assembly: AssemblyDescription("用于监视目录，自动把所监视的目录里新增的 webp 文件转换为 jpeg/png 文件")]
+[assembly: AssemblyDescription("监视目录并自动把目录里新增或被重命名为目标格式的图像转换为指定格式")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("hrpzcf")]
 [assembly: AssemblyProduct("ImageCvt")]
@@ -51,5 +51,5 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
